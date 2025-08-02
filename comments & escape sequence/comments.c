8172446ead451f9b01e8473dd7f651f7096940ab
vinci999 <124420382+vinciCode999@users.noreply.github.com>
2025-08-02 11:23:57 +0200

    lesson2: comments and escape sequence

---
+++ comments & escape sequence/comments.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+
+int main(){
+  //This line is a comment
+  
+  /*
+    This is a multline comment in C
+    comments are ignored by a compiler
+  */
+ printf("learning comments in c");
+ return 0;
+}
