7f239955c7f446d8bdea37b80fb7ea7b4f2ebd2a
vinci999 <124420382+vinciCode999@users.noreply.github.com>
2025-08-02 19:07:39 +0200

    lesson 8: augmented operators

---
+++ augmented assignment operators/augmentedAssignmentOperators.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+  /*
+    AUGMENTED ASSIGNMENT OPERATORS = use to replace a statement where an operator takes a variable as one of its arguments
+                                     and then assigns the result back to the same variable.
+                                     x = x + 1
+                                     x += 1
+  */
+
+  int x = 10;
+  // x+=2;
+  // x -= 3;
+  // x *= 2;
+  // x %= 5;
+  // x /= 5;
+
+  printf("%d", x);
+  return 0;
+}
