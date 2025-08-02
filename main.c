c6866b13399bc4d1637b2a89cbcd6a85479554bc
vinci999 <124420382+vinciCode999@users.noreply.github.com>
2025-08-02 21:22:16 +0200

    hypotenuse calculations

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(){
+
+  double opposite;
+  double adjacent;
+  double hypotenuse;
+
+  printf("\nEnter value of opposite side's value of triangle: ");
+  scanf("%lf", &opposite);
+
+  printf("\nEnter adjacent side's value of triangle: ", adjacent);
+  scanf("%lf", &adjacent);
+
+  // hypotenuse = sqrt(pow(opposite, 2)+pow(adjacent, 2));
+  hypotenuse = sqrt(opposite*opposite + adjacent*adjacent);
+  printf("\nThe hypotenuse of the triangle is %lf", hypotenuse);
 
   return 0;
 }
