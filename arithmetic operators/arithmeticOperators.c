198221bfdc763f0391b91eedeb783c2d0e50ce1b
vinci999 <124420382+vinciCode999@users.noreply.github.com>
2025-08-02 19:00:20 +0200

    lesson 7: arithmetic operators

---
+++ arithmetic operators/arithmeticOperators.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+  //arithmetic operators
+
+  // + (addition)
+  // - (subtraction)
+  // / (division)
+  // * (multiplication)
+  // % (modulus)
+  // ++ (increment)
+  // -- (decrement)
+
+  int x = 5;
+  int y = 2;
+
+  // float z = x / (float) y;
+  // int z = x%y;
+  // x++;
+  // y--; 
+  
+  return 0;
+}
--- main.c
+++ main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
-#include <stdbool.h>
 
 int main(){
-  
+ 
   return 0;
 }
