743a61e78eec6e7b2fa14932bd02c310f8b509c2
vinci999 <124420382+vinciCode999@users.noreply.github.com>
2025-08-02 18:20:43 +0200

    lesson 6: constants

---
+++ constants/constants.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main(){
+  //constant = fixed value that can not be altered by the program during its execution
+  const float PI = 3.141592;
+  printf("%f", PI);
+  return 0;
+}
