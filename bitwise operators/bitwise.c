850b5003d17d2bb67ec98c89c0228286e914b3a4
vinci999 <124420382+vinciCode999@users.noreply.github.com>
2025-08-31 11:23:08 +0200

    bitwise operators

---
+++ bitwise operators/bitwise.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <ctype.h>
+
+int main() {
+
+  //BITWISE OPERATORS = special operators used bit in level programming 
+  //                    (knowing binary is important for this topic)
+
+  //& = AND
+  //| = OR
+  //^ = XOR
+  //~ = NOT
+  //<< = left shift
+  //>> = right shift
+
+  int x = 6;  // 00000110
+  int y = 12; // 00001100
+  int z = 0; //  00001010
+
+  z = x & y;
+  printf("AND = %d\n", z);
+
+
+  z = x | y;
+  printf("OR = %d\n", z);
+
+  
+  z = x ^ y;
+  printf("XOR = %d", z);
+
+  z = x << 1; //formula 6*2(n=number of shift in ourcase 1), 6 based on our example
+  printf("\nleft bitwise: %d\n", z);
+
+  z = x >> 1; //formula 6/2(n=number of shift in ourcase 1), 6 based on our example
+  printf("right bitwise: %d\n", z);
+  return 0;
+}
--- main.c
+++ main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
-#include <ctype.h>
 
 int main() {
 
   return 0;
-
 }
